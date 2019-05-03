e071a9575ae8ff2c1bcac2785a73b0d8a82e7038
ChrstphGrnr <ChristophGruener@me.com>
2019-05-03 14:42:34 -0600

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
+    @comment = Comment.new
+    @comment.build_user
+    @comment.post = @post
+    @users = @post.users.distinct
   end
 
   def index
@@ -9,6 +13,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @post.categories.build
   end
 
   def create
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user, reject_if: :all_blank
+
+  def user_attributes=(attribute)
+    self.user = User.find_or_create_by(username: attribute["username"]) unless attribute["username"].empty?
+  end
+
+
 end
